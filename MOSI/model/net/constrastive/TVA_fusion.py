636f6aab34aba7e9446eabb60eca9a21e0a8bda4
AAAAAAA <jiuding@ualberta.ca>
2024-04-01 10:43:55 +0800

    Update TVA_fusion.py

--- MOSI/model/net/constrastive/TVA_fusion.py
+++ MOSI/model/net/constrastive/TVA_fusion.py
@@ -149,7 +149,7 @@ class TVA_fusion(nn.Module):
             sup_const_loss = 0
             # sds_loss = 0
             if sample2 is not None:
-                # [Ts,T1s,T2s,T3s,T4s,T5s,T6s,V1s,V2s,V3s,....]
+                # For sequence [Ts, Vs, As, Td, Vd, Ad, T1s, V1s, ...... V6d, A6d], construct corresponding positive and negative embedding pairs, which is use for contrastive learning later.
                 t1, p, t2, n = torch.tensor([0, 0, 7, 7, 14, 14,
                                              0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6],
                                             device=device), \
