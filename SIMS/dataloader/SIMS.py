ed974492c9f539df4fd41de4bc3b7c40e4327b1d
Pasta009 <jiuding@ualberta.ca>
2024-06-06 16:53:11 +0800

    Update SIMS.py
    fix bug in building retireve index

--- SIMS/dataloader/SIMS.py
+++ SIMS/dataloader/SIMS.py
@@ -158,15 +158,15 @@ class SIMSDataset(Dataset):
             for j in range(int(self.size/2)):
                 if i == j: continue
                 if _label[i] == _label[int(_rank[i][j])]:
-                    _ss.append(j)
+                    _ss.append(int(_rank[i][j]))
                 else:
-                    _sd.append(j)
+                    _sd.append(int(_rank[i][j]))
             for j in range(-1, -int(self.size/2), -1):
                 if i == j: continue
                 if _label[i] == _label[int(_rank[i][j])]:
-                    _ds.append(j)
+                    _ds.append(int(_rank[i][j]))
                 else:
-                    _dd.append(j)
+                    _dd.append(int(_rank[i][j]))
             if len(_ss) < 2 or len(_sd) < 2 or len(_ds) < 2 or len(_dd) < 2:
                 print('Unique sample detected, may cause error!')
 
